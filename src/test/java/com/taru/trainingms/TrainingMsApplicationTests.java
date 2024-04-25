0c14b37a908f0b96b6be42833d723883222ac999
2taru <turianytsia.timur@gmail.com>
2024-04-25 14:26:45 +0300

    feat: basic project configuration

---
+++ src/test/java/com/taru/trainingms/TrainingMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taru.trainingms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
